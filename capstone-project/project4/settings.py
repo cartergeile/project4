9ae758ee129ccaf87f6640bc04a04986ebd8deaf
Carter Geile <cartergeile@l.local>
2023-03-14 11:51:01 -0500

    added a password reset feature via email

--- capstone-project/project4/settings.py
+++ capstone-project/project4/settings.py
@@ -119,10 +119,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # MEDIA_URL = ''
 # MEDIA_ROOT = BASE_DIR/''
-LOGIN_REDIRECT_URL = '/profile/'
+LOGIN_REDIRECT_URL = '/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
