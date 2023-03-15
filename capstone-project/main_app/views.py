818045e6bf0d5e7fe8ff366e453cd88c14242016
Carter Geile <cartergeile@l.local>
2023-03-14 23:24:26 -0500

    finishing touches are done

--- capstone-project/main_app/views.py
+++ capstone-project/main_app/views.py
@@ -8,6 +8,7 @@ from . forms import CustomerProfileForm, CustomerRegistrationForm
 from django.contrib import messages
 from django.db.models import Q
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
